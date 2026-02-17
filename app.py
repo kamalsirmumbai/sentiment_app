851d11de756feeb0ce269d2e1c6c96662f09e76e
kamalclassmumbai <130304049+kamalclassmumbai@users.noreply.github.com>
2026-02-17 13:04:09 +0530

    add nltk download

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 from flask import *
-from  nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+nltk.download("vader_lexicon")
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-	if request.method == "POST":
-		review = request.form.get("review")
-		sia = SentimentIntensityAnalyzer()	
-		ps = sia.polarity_scores(review)
-		feedback = ""
-		if ps["compound"] >= 0.05:
-			feedback = "thanks for positive review"
-		elif ps["compound"] <= -0.05:
-			feedback = "sorry for ur negative review "
-		else:
-			feedback = "neutral"
-		msg = " got it " + feedback 
-		return render_template("home.html", msg=msg)
-	else:
-		return render_template("home.html")
-		
-# app.run(debug=True, use_reloader=True)
+    if request.method == "POST":
+        review = request.form.get("review")
+        sia = SentimentIntensityAnalyzer()	
+        ps = sia.polarity_scores(review)
+
+        if ps["compound"] >= 0.05:
+            feedback = "thanks for positive review"
+        elif ps["compound"] <= -0.05:
+            feedback = "sorry for your negative review"
+        else:
+            feedback = "neutral"
+
+        msg = "Got it: " + feedback 
+        return render_template("home.html", msg=msg)
+    else:
+        return render_template("home.html")
